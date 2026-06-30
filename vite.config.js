b4cea812368a5a2769f4483ed72c4521ffc6fd67
mrsstephanieclark-spec <mrsstephanieclark-spec@users.noreply.github.com>
2026-06-30 16:25:35 -0400

    feat: Add Contact page

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,17 @@
 import { defineConfig } from 'vite';
+import { resolve } from 'path';
 
 export default defineConfig({
   root: '.',
   build: {
     outDir: 'dist',
     emptyOutDir: true,
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        contact: resolve(__dirname, 'contact/index.html')
+      }
+    }
   },
   server: {
     port: 3000,
